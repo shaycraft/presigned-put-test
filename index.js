1b1c4204dfd2f15e046acd194a08a15220a91c19
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-12-19 16:32:43 -0700

    feat: Pre-signed URL call

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-async function main() {}
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-west-2' });
+const s3 = new AWS.S3();
 
-main().then(() => {
-  console.debug("Program finished");
+async function main() {
+  const bucketName = 'sam-testing-presigned-put-test';
+
+  const key = 'test-upload-put-file.jpg';
+
+  const s3Params = {
+    Bucket: bucketName,
+    Key: key,
+    Expires: 300, // seconds
+    ContentType: 'image/jpeg',
+  };
+
+  return await s3.getSignedUrlPromise('putObject', s3Params);
+}
+
+main().then((url) => {
+  console.debug('Program finished, url = ');
+  console.debug(url);
 });
